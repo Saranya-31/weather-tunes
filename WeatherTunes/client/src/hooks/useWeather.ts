cecf99e8b0ff2d5b21282c1e6065178af70c1120
Saranya-31 <165178510+Saranya-31@users.noreply.github.com>
2025-07-22 18:36:05 +0530

    Delete WeatherTunes directory

--- WeatherTunes/client/src/hooks/useWeather.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { fetchWeather } from "@/lib/weatherApi";
-
-export function useWeather() {
-  return useMutation({
-    mutationFn: fetchWeather,
-  });
-}
